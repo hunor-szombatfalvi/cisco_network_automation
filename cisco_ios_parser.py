cf7c959bdcac15e422cb187be097e827b90c1819
Szombatfalvi Hunor Gellért <evilgoat.bmf@gmail.com>
2016-05-24 10:08:33 +0300

    Update cisco_ios_parser.py

--- cisco_ios_parser.py
+++ cisco_ios_parser.py
@@ -1,4 +1,5 @@
 def port_channel(ip_address,dev=0):
+
     global list_of_portchannels_json
     import ssh
     if dev != 0:
@@ -10,6 +11,8 @@ def port_channel(ip_address,dev=0):
             # parses the output line by line, delimits variables and collects all of them in a list
             full_list = []
             for line in show_port_channel_sum.split('\n'):
+#                if "}" in line and "username" in line:
+#                    pref_cred = line
                 if len([character for character in line if character.isdigit()]) > 0 and "(" in line:
                     segmented_line = (' '.join(line.split(")")).split())
                     full_list += segmented_line
@@ -59,16 +62,12 @@ def port_channel(ip_address,dev=0):
             print ("[[DEV:] General exception triggered in cisco.port_channel")
             continue
 
-    if dev != 0 and list_of_portchannels_json != {}:
-        print("[[DEV:] Successful]")
+    if dev != 0:
         print("[[DEV:] Getting hostname]")
-
     for retries in range(0, 3):
         try:
             hostname = ssh.hostname_silent(ip_address=ip_address, dev=dev)
             output = {hostname: list_of_portchannels_json}
-            if dev != 0 and list_of_portchannels_json != {}:
-                print("[[DEV:] Successful]")
             return output
 
         except ssh.SSHnotEnabled:
@@ -76,5 +75,66 @@ def port_channel(ip_address,dev=0):
             break
 
         except Exception:
-            print ("[[DEV:] General exception triggered in cisco.hostname")
+            print ("[[DEV:] General exception triggered in cisco.port_channel")
+            continue
+
+
+def cdp_neighbor(ip_address, dev=0):
+    import ssh
+    if dev != 0:
+        print("[[DEV:] Getting CDP neighbor information]")
+    for retries in range(0, 3):
+        try:
+            show_cdp_ne_de = ssh.connect_enable_silent('show cdp neighbors detail', ip_address=ip_address, dev=1)
+            split_cdp = show_cdp_ne_de.split('\n')
+            network_devices = {}
+
+            for line in split_cdp:
+                if '----------------' in line:
+                    hostname = ''
+                if 'Device ID:' in line:
+                    (junk, hostname) = line.split('Device ID:')
+                    hostname = hostname.strip()
+                    if '.' in hostname:
+                        hostname = hostname[0:hostname.find('.')]
+
+                    if not hostname in network_devices:
+                        network_devices[hostname] = {}
+
+                if 'IP address:' in line:
+                    (junk, ip) = line.split('IP address:')
+                    ip = ip.strip()
+                    network_devices[hostname]['ip'] = ip
+                elif 'IPv4 Address: ' in line:
+                    (junk, ip) = line.split('IPv4 Address:')
+                    ip = ip.strip()
+                    network_devices[hostname]['ip'] = ip
+
+                if 'Platform:' in line:
+                    (platform, capabilities) = line.split(',')
+                    (junk, model) = platform.split("Platform:")
+                    model = model.strip()
+                    network_devices[hostname]['model'] = model
+
+                    (junk, capabilities) = capabilities.split("Capabilities: ")
+                    if 'Router' in capabilities:
+                        device_type = 'router'
+                    elif 'Switch' in capabilities:
+                        device_type = 'switch'
+                    elif 'Phone' in capabilities:
+                        device_type = 'phone'
+                    elif 'Trans-Bridge' in capabilities:
+                        device_type = 'wireless access point'
+                    else:
+                        device_type = 'unknown'
+                    network_devices[hostname]['model'] = model
+                    network_devices[hostname]['device_type'] = device_type
+            return network_devices
+
+        except ssh.SSHnotEnabled:
+            print("[[DEV:] Future: Raise error for different module or pass to Telnet")
+            break
+
+        except Exception:
+            print("[[DEV:] General exception triggered in cisco.port_channel")
             continue
