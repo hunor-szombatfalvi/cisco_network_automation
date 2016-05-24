17f447c72bdf97d003b4a729ae09f346b19e23a0
Szombatfalvi Hunor Gellért <evilgoat.bmf@gmail.com>
2016-05-24 10:08:07 +0300

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,6 +1,8 @@
 import netmiko
 from netmiko import ConnectHandler
+import globals
 import json
+
 
 class SSHnotEnabled(ConnectionError):
     pass
@@ -17,24 +19,37 @@ def connect_enable_silent(*ios_commands,ip_address,dev=0):
     try:
         with open ("credentials.txt") as line:
             line = json.load(line)
+            try_credentials = 0
             for k,v in line.items():
                 router=(k,v)
+                try_credentials += 1
                 try:
-                    if dev != 0:
-                        print("[[DEV:] Trying Privileged EXEC credentials '", k,"']",sep="")
-                    ssh = ConnectHandler(**router[1],device_type="cisco_ios",ip=ip_address)
-                    ssh.enable()
+                    if globals.pref_cred != {} and try_credentials == 1:
+                        if dev != 0:
+                            print("[[DEV:] Trying Prefered credentials]")
+                        ssh = ConnectHandler(**globals.pref_cred, device_type="cisco_ios", ip=ip_address)
+                        ssh.enable()
+                    else:
+                        if dev != 0:
+                            print("[[DEV:] Trying Privileged EXEC credentials '", k, "']", sep="")
+                        ssh = ConnectHandler(**router[1], device_type="cisco_ios", ip=ip_address)
+                        ssh.enable()
                 except netmiko.ssh_exception.NetMikoAuthenticationException:
                     if dev != 0:
                         print ("[[DEV:] Incorrect credentials]")
                     continue
                 except netmiko.ssh_exception.NetMikoTimeoutException:
-                    if dev != 0:
-                        print("[[DEV:] Trying User EXEC credentials (Privileged EXEC timed out)]")
                     # oddly enough if it can log in but not able to authenticate to enable mode,
                     # the ssh.enable() command does not give an authentication error but a time-out error instead
                     try:
-                        ssh = ConnectHandler(username=router[1]['username'], password=router[1]['password'],
+                        if globals.pref_cred != {} and try_credentials == 1:
+                            if dev != 0:
+                                print("[[DEV:] Trying User EXEC Prefered credentials (Privileged EXEC timed out)]")
+                            ssh = ConnectHandler(**globals.pref_cred, device_type="cisco_ios", ip=ip_address)
+                        else:
+                            if dev != 0:
+                                print("[[DEV:] Trying User EXEC credentials (Privileged EXEC timed out)]")
+                            ssh = ConnectHandler(username=router[1]['username'], password=router[1]['password'],
                                              device_type="cisco_ios", ip=ip_address)
                     except netmiko.ssh_exception.NetMikoTimeoutException:
                         if dev != 0:
@@ -53,6 +68,9 @@ def connect_enable_silent(*ios_commands,ip_address,dev=0):
                                 print("[[DEV:] '", ios_command, "' incorrect syntax or requires enable mode]", sep="")
                             if "at '^' marker" not in output:
                                 ssh.disconnect()
+                                if dev != 0 and globals.pref_cred == {} or dev != 0 and globals.pref_cred != {} and try_credentials > 1:
+                                    print("[[DEV:] Saving '", k, "' as prefered credentials]", sep="")
+                                    globals.pref_cred = v
                                 break
                         if "at '^' marker" not in output:
                             return output
@@ -74,39 +92,57 @@ def connect_enable_silent(*ios_commands,ip_address,dev=0):
                             break
                     if "at '^' marker" in output:
                         raise IosSyntaxError
+                    if dev != 0 and globals.pref_cred == {} or dev != 0 and globals.pref_cred != {} and try_credentials > 1:
+                        print("[[DEV:] Saving '", k, "' as prefered credentials]", sep="")
+                        globals.pref_cred = v
                     return output
     except json.decoder.JSONDecodeError:
         if dev != 0:
             print("[[DEV:] credentials file not in JSON format]")
     raise JsonIncorrectFormat ("Credentials file not in JSON format")
-    
-    
+
+
 def hostname_silent(ip_address,dev=0):
-    with open ("credentials.txt") as line:
-        line_1 = json.load(line)
-        for k,v in line_1.items():
-            router=(k,v)
-            try:
-                if dev != 0:
-                    print("[[DEV:] Trying Privileged EXEC credentials '", k, "']", sep="")
-                ssh = ConnectHandler(**router[1],device_type="cisco_ios",ip=ip_address)
-            except netmiko.ssh_exception.NetMikoAuthenticationException:
-                if dev != 0:
-                    print("[[DEV:] Incorrect credentials]")
-                continue
-            except netmiko.ssh_exception.NetMikoTimeoutException:
-                if dev != 0:
-                    print("[[DEV:] SSH not enabled (User EXEC timed out)]")
-                raise SSHnotEnabled("SSH not enabled on target device (" + ip_address + ")") from None
-            except Exception:
-                if dev != 0:
-                    print("[[DEV:] Unknown error in ssh.hostname_silent]")
-                raise UnknownError("Unknown error in ssh.hostname_silent")
-            else:
-                output = ssh.find_prompt()
-                if ('#') in output:
-                    (output_split,junk) = output.split('#')
+    try:
+        with open ("credentials.txt") as line:
+            line_1 = json.load(line)
+            try_credentials = 0
+            for k,v in line_1.items():
+                router=(k,v)
+                try_credentials += 1
+                try:
+                    if globals.pref_cred != {} and try_credentials == 1:
+                        if dev != 0:
+                            print("[[DEV:] Trying Prefered credentials]")
+                        ssh = ConnectHandler(**globals.pref_cred, device_type="cisco_ios", ip=ip_address)
+                    else:
+                        if dev != 0:
+                            print("[[DEV:] Trying Privileged EXEC credentials '", k, "']", sep="")
+                        ssh = ConnectHandler(**router[1],device_type="cisco_ios",ip=ip_address)
+                except netmiko.ssh_exception.NetMikoAuthenticationException:
+                    if dev != 0:
+                        print("[[DEV:] Incorrect credentials]")
+                    continue
+                except netmiko.ssh_exception.NetMikoTimeoutException:
+                    if dev != 0:
+                        print("[[DEV:] SSH not enabled (User EXEC timed out)]")
+                    raise SSHnotEnabled("SSH not enabled on target device (" + ip_address + ")") from None
+                except Exception:
+                    if dev != 0:
+                        print("[[DEV:] Unknown error in ssh.hostname_silent]")
+                    raise UnknownError("Unknown error in ssh.hostname_silent")
                 else:
-                    (output_split, junk) = output.split('>')
-                ssh.disconnect()
-                return output_split
+                    output = ssh.find_prompt()
+                    if ('#') in output:
+                        (output_split,junk) = output.split('#')
+                    else:
+                        (output_split, junk) = output.split('>')
+                    ssh.disconnect()
+                    if dev != 0 and globals.pref_cred == {} or dev != 0 and globals.pref_cred != {} and try_credentials > 1:
+                        print("[[DEV:] Saving '", k, "' as prefered credentials]", sep="")
+                        globals.pref_cred = v
+                    return output_split
+    except json.decoder.JSONDecodeError:
+        if dev != 0:
+            print("[[DEV:] credentials file not in JSON format]")
+    raise JsonIncorrectFormat("Credentials file not in JSON format")
